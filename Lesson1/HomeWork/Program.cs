756cbeb1eb4f95686f4fe5562a963429be15ee53
Anton_Python <ogarkov.anton91@yandex.ru>
2024-12-30 10:28:15 +0700

    Add HomeWork

---
+++ Lesson1/HomeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿//Лекция 1. Знакомство с языком программирования C#
+
+int a = 12;
+int b = 2;
+int product = (a * b);
+
+int c = 2;
+int d = 3;
+int sum = (c + d);
+
+Console.WriteLine(product / sum);
