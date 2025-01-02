c8d1140b50cbccf21e1893a0f69c7e255040d351
Anton_Python <ogarkov.anton91@yandex.ru>
2025-01-02 15:32:05 +0700

    Added HomeWork1

---
+++ Seminar1/HomeWork1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (secondNumber == 0)
+    Console.WriteLine("на ноль делить нельзя");
+else if (firstNumber % secondNumber == 0)
+    Console.WriteLine("делится");
+else
+    Console.WriteLine("не делится");
