782e6e777e928487771c47dbb3c515ac235346e0
Anton_Python <ogarkov.anton91@yandex.ru>
2025-01-03 14:43:32 +0700

    Added Seminar1:homeWork1

---
+++ Seminar2/homeWork1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine("Не кратно");
