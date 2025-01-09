36fca3e92e8743ee86021c7c8840308cd7857f40
Anton_Python <ogarkov.anton91@yandex.ru>
2025-01-09 10:15:40 +0700

    Added Lesson2:Task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 24, 4, 7, 35, 9 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
